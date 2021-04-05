9432e1a9c436229e9ee73d6f11235fee4ed917b7
Roee Weiss Lipshitz <39015778+Roeezz@users.noreply.github.com>
2021-04-05 17:21:36 +0300

    Add tool for cleaning up data.

---
+++ clean_data_tool.py
@@ -0,0 +1,71 @@
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.metrics import structural_similarity, mean_squared_error
+
+
+def find_box_cords(a):
+    r = a.any(1)
+    if r.any():
+        m, n = a.shape
+        c = a.any(0)
+        out = (r.argmax(), m - r[::-1].argmax(), c.argmax(), n - c[::-1].argmax())
+    else:
+        out = (0, 0, 0, 0)
+    return out
+
+
+if __name__ == '__main__':
+    im_count = 0
+    total = 0
+    strip_len = 0
+    strip_num = 0
+    dirr = 'image_strips/TROOP/'
+    for filename in os.listdir(dirr):
+        if filename.endswith('.npy'):
+            img, flow, bbox, data = np.load(dirr + filename, allow_pickle=True)
+            total += len(img)
+            for i in range(len(img) - 1):
+                xA, x_wA, yA, y_hA = find_box_cords(bbox[i][:, :, 0])
+                xB, x_wB, yB, y_hB = find_box_cords(bbox[i + 1][:, :, 0])
+                imageA = img[i][xA:x_wA, yA:y_hA]
+                imageB = img[i + 1][xB:x_wB, yB:y_hB]
+
+                ratioA = imageA.shape[0] / imageA.shape[1]
+                ratioB = imageB.shape[0] / imageB.shape[1]
+
+                interpolation = cv2.INTER_CUBIC if imageA.shape[0] > imageB.shape[0] else cv2.INTER_AREA
+                imageB = cv2.resize(imageB, (imageA.shape[1], imageA.shape[0]), interpolation=interpolation)
+                grayA = cv2.cvtColor(imageA, cv2.COLOR_RGB2GRAY)
+                grayB = cv2.cvtColor(imageB, cv2.COLOR_RGB2GRAY)
+
+                # compute the Structural Similarity Index (SSIM) between the two
+                # images, ensuring that the difference image is returned
+                try:
+                    (score, _) = structural_similarity(grayA, grayB, full=True)
+                except ValueError:
+                    print(f'grayA.shape: {grayA.shape} grayB.shape{grayB.shape}')
+                    continue
+                MSE = mean_squared_error(grayA, grayB)
+                if strip_len == 9:
+                    strip_num += 1
+                    strip_len = 0
+
+                if MSE < 200:
+                    strip_len += 1
+                    im_count += 1
+                    # f, axarr = plt.subplots(1, 2)
+                    # axarr[0].imshow(imageA)
+                    # axarr[0].set_title('A for ass')
+                    # axarr[1].imshow(imageB)
+                    # axarr[1].set_title('B for butt')
+                    # f.text(0.5, 0.04,
+                    #        f'-SSIM:{score:.5f} MSE:{MSE:.2f}\nratio A:{ratioA} ratio B:{ratioB}',
+                    #        ha='center', va='center', size='medium')
+                    # plt.show()
+                else:
+                    strip_len = 0
+
+    print(f'total: {total} im_count:{im_count} strip_num:{strip_num}')
